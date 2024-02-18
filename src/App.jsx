eb9987c3dc4da461221b65d02318e7f7309558c6
Shirley Nguyen <shirley.nguyen8@outlook.com>
2024-02-17 19:00:24 -0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return <Header></Header>;
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+const Header = () => {
+  return <div style={{ border: "2px solid pink", minHeight: "200px" }}></div>;
+};
+
+export default Header;
